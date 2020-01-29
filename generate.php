df591af913a45e64c740f1a5c62ee51223c90fcd
Yanick <rt-2@rt-2.net>
2020-01-29 02:11:27 -0500

    Update generate.php
    Protect the script from errors/warning before a header

--- generate.php
+++ generate.php
@@ -2,12 +2,9 @@
 
 $kml = new SimpleXMLElement($_POST['kml']);
 
-$kml = $kml->Document->Folder->Folder;
+@$kml = $kml->Document->Folder->Folder;
 
-Header("Content-Type: text/plain");
-Header("Content-Disposition: attachment; filename=" . $_POST['icao'] . ".apt");
-
-foreach ($kml as $folder)
+@foreach ($kml as $folder)
 {
     switch ($folder->name) {
         case 'Runways':
@@ -27,6 +24,9 @@ foreach ($kml as $folder)
             break;
     }
 }
+
+Header("Content-Type: text/plain");
+Header("Content-Disposition: attachment; filename=" . $_POST['icao'] . ".apt");
 
 echo "icao=" . $_POST['icao'] . "\n";
 echo "magnetic variation=" . $_POST['magneticVariation'] . "\n";
